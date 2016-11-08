b818d9fc8cb2af7ae9e1f4f247bb04ff478813de
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-08 18:17:21 +0100

    Remove stop setter

--- app/src/main/java/com/luca020400/amt/Stop.java
+++ app/src/main/java/com/luca020400/amt/Stop.java
@@ -8,42 +8,26 @@ class Stop implements Serializable {
     private String schedule;
     private String remainingtime;
 
-    public Stop(String line, String destination, String schedule, String remainingtime) {
+    Stop(String line, String destination, String schedule, String remainingtime) {
         this.line = line;
         this.destination = destination;
         this.schedule = schedule;
         this.remainingtime = remainingtime;
     }
 
-    public String getLine() {
+    String getLine() {
         return line;
     }
 
-    public void setLine(String line) {
-        this.line = line;
-    }
-
-    public String getDestination() {
+    String getDestination() {
         return destination;
     }
 
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getSchedule() {
+    String getSchedule() {
         return schedule;
     }
 
-    public void setSchedule(String schedule) {
-        this.schedule = schedule;
-    }
-
-    public String getRemainingtime() {
+    String getRemainingtime() {
         return remainingtime;
-    }
-
-    public void setRemainingtime(String remainingtime) {
-        this.remainingtime = remainingtime;
     }
 }
