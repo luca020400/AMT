7bd0e33ed0938466e2d5e0393c2059c03f163d26
Luca Stefani <luca020400@lineageos.org>
2017-07-07 23:30:42 +0200

    AMT: Get rid of data binding

--- app/src/main/kotlin/com/luca020400/amt/StopAdapter.kt
+++ app/src/main/kotlin/com/luca020400/amt/StopAdapter.kt
@@ -1,10 +1,10 @@
 package com.luca020400.amt
 
-import android.databinding.DataBindingUtil
-import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.stop_adapter.view.*
 
 internal class StopAdapter : RecyclerView.Adapter<StopAdapter.StopViewHolder>() {
     private val stops = arrayListOf<StopData>()
@@ -18,18 +18,23 @@ internal class StopAdapter : RecyclerView.Adapter<StopAdapter.StopViewHolder>() 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StopViewHolder {
-        val binding = DataBindingUtil.inflate<ViewDataBinding>(LayoutInflater.from(parent.context),
-                R.layout.stop_adapter, parent, false)
-        return StopViewHolder(binding)
+        val v = LayoutInflater.from(parent.context)
+                .inflate(R.layout.stop_adapter, parent, false)
+        return StopViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: StopViewHolder, position: Int) {
-        holder.viewDataBinding.setVariable(BR.stop, stops[holder.adapterPosition])
-        holder.viewDataBinding.executePendingBindings()
+        holder.bindStop(stops[holder.adapterPosition])
     }
 
     override fun getItemCount() = stops.size
 
-    class StopViewHolder(val viewDataBinding: ViewDataBinding) :
-            RecyclerView.ViewHolder(viewDataBinding.root)
+    class StopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindStop(stopData: StopData) {
+            itemView.line.text = stopData.line
+            itemView.eta.text = stopData.remaining_time
+            itemView.destination.text = stopData.destination
+            itemView.schedule.text = stopData.schedule
+        }
+    }
 }
